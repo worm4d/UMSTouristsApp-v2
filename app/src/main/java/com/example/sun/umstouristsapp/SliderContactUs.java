939531e28354cbf27f68df07458f4c7bdf150844
sun <worm-4d@hotmail.com>
2017-03-06 00:42:18 +0800

    updated info on Contact Us page
    completed the click listeners on Contact Us page

--- app/src/main/java/com/example/sun/umstouristsapp/SliderContactUs.java
+++ app/src/main/java/com/example/sun/umstouristsapp/SliderContactUs.java
@@ -1,17 +1,52 @@
 package com.example.sun.umstouristsapp;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class SliderContactUs extends AppCompatActivity {
-    ImageView backBtn;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.sun.umstouristsapp.R.id.map;
+
+public class SliderContactUs extends AppCompatActivity implements OnMapReadyCallback{
+    ImageView backBtn, fbIcon;
+    ImageView contactCall, contactEmail, contactLocation;
+    private GoogleMap googleMap;
+    LatLngBounds.Builder builder;
+    CameraUpdate cu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider_contact_us);
+
+        fbIcon = (ImageView) findViewById(R.id.icon_fb);
+        fbIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.facebook.com/ecocampusums/"));
+                startActivity(intent);
+            }
+        });
 
         backBtn = (ImageView) findViewById(R.id.backImage);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -20,5 +55,110 @@ public class SliderContactUs extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        contactCall = (ImageView) findViewById(R.id.contact_call);
+        contactCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:088320000"));
+                startActivity(intent);
+            }
+        });
+
+        contactLocation = (ImageView) findViewById(R.id.contact_location);
+        contactLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SliderContactUs.this, EvicUms_map.class);
+                startActivity(intent);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+
+        contactEmail = (ImageView) findViewById(R.id.contact_email);
+        contactEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address[] = {"ecocampus@ums.edu.my"};
+                String subject = "Inquiring about your services";
+                composeEmail(address, subject );
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        googleMap = map;
+
+        mSetUpMap();
+    }
+
+    /**create method to set map view*/
+    public void mSetUpMap() {
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setTrafficEnabled(true);
+        googleMap.setIndoorEnabled(true);
+        googleMap.setBuildingsEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        /**clear the map before redraw to them*/
+        googleMap.clear();
+        /**Create dummy Markers List*/
+        List<Marker> markersList = new ArrayList<>();
+        Marker EVIC = googleMap.addMarker(new MarkerOptions().position(new LatLng(
+                6.032509, 116.121645)).title("EcoCampus Visitor Information Centre"));
+
+        /**Put all the markers into arraylist*/
+        markersList.add(EVIC);
+
+        /**create for loop for get the latLngbuilder from the marker list*/
+        builder = new LatLngBounds.Builder();
+        for (Marker m : markersList) {
+            builder.include(m.getPosition());
+        }
+        /**initialize the padding for map boundary*/
+        int padding = 50;
+        /**create the bounds from latlngBuilder to set into map camera*/
+        LatLngBounds bounds = builder.build();
+        /**create the camera with bounds and padding to set into map*/
+        cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        /**call the map call back to know map is loaded or not*/
+        googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                /**set animated zoom camera into map*/
+                googleMap.moveCamera(cu);
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        6.032509, 116.121645),17));
+
+            }
+        });
+    }
+
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
