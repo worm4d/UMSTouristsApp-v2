ff353c8cf639818e24fb9b4160c6404d280e1c43
sun <worm-4d@hotmail.com>
2017-03-22 21:23:43 +0800

    removed view pager library

--- app/src/main/java/com/example/sun/umstouristsapp/MainActivity.java
+++ app/src/main/java/com/example/sun/umstouristsapp/MainActivity.java
@@ -5,13 +5,9 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,22 +19,14 @@ import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
-import com.viewpagerindicator.CirclePageIndicator;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends RuntimePermissionsActivity{
 
@@ -46,13 +34,7 @@ public class MainActivity extends RuntimePermissionsActivity{
     Button camera, map;
     Toolbar toolbar;
     ImageView imageView, menu;
-    private static ViewPager mPager;
     private int REQUEST_CAMERA = 0;
-    private static int currentPage = 0;
-    private static int NUM_PAGES = 0;
-
-    int IMAGES[] = {R.drawable.ad_1, R.drawable.ad_2, R.drawable.ad_3, R.drawable.ad_4, R.drawable.ad_5, };
-    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
     private static final int REQUEST_PERMISSIONS = 20;
 
@@ -64,13 +46,10 @@ public class MainActivity extends RuntimePermissionsActivity{
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         toolbar.setTitleTextColor(-1);
 
-
-        init();
         clickMenu();
 
         newAttraction = (Button) findViewById(R.id.main_attraction);
         newFacility = (Button) findViewById(R.id.main_facility);
-//        newAdmin = (Button) findViewById(R.id.main_admin);
         newPackage = (Button) findViewById(R.id.main_packages);
         newEVIC = (Button) findViewById(R.id.main_evic);
         camera = (Button) findViewById(R.id.main_camera);
@@ -92,14 +71,6 @@ public class MainActivity extends RuntimePermissionsActivity{
                 startActivity(intent);
             }
         });
-
-//        newAdmin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this,Admin.class);
-//                startActivity(intent);
-//            }
-//        });
 
         newPackage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,20 +101,8 @@ public class MainActivity extends RuntimePermissionsActivity{
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
-//                // Create a Uri from an intent string. Use the result to create an Intent.
-//                Uri gmmIntentUri = Uri.parse("geo:<6.0362>,<116.1188>(Label+Name)");
-//
-//                // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-//                // Make the Intent explicit by setting the Google Maps package
-//                mapIntent.setPackage("com.google.android.apps.maps");
-//
-//                // Attempt to start an activity that can handle the Intent
-//                startActivity(mapIntent);
             }
         });
-
-
 
 
     }
@@ -198,6 +157,12 @@ public class MainActivity extends RuntimePermissionsActivity{
     }
 
     private void clickMenu() {
+
+        MainActivity.super.requestAppPermissions(new
+                        String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, R.string
+                        .runtime_permissions_txt
+                , REQUEST_PERMISSIONS);
 
         menu = (ImageView) findViewById(R.id.menu);
         menu.setColorFilter(-1);
@@ -280,75 +245,6 @@ public class MainActivity extends RuntimePermissionsActivity{
             @Override
             public void onClick(View v) {
                 result.openDrawer();
-            }
-        });
-
-    }
-
-    private void init() {
-
-        MainActivity.super.requestAppPermissions(new
-                        String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION}, R.string
-                        .runtime_permissions_txt
-                , REQUEST_PERMISSIONS);
-
-        for(int i=0;i<IMAGES.length;i++)
-            ImagesArray.add(IMAGES[i]);
-
-        mPager = (ViewPager) findViewById(R.id.pager);
-
-
-        mPager.setAdapter(new SlidingImage_Adapter(MainActivity.this,ImagesArray));
-
-
-        CirclePageIndicator indicator = (CirclePageIndicator)
-                findViewById(R.id.indicator);
-
-        indicator.setViewPager(mPager);
-
-        final float density = getResources().getDisplayMetrics().density;
-
-//Set circle indicator radius
-        indicator.setRadius(5 * density);
-
-        NUM_PAGES =IMAGES.length;
-
-        // Auto start of viewpager
-        final Handler handler = new Handler();
-        final Runnable Update = new Runnable() {
-            public void run() {
-                if (currentPage == NUM_PAGES) {
-                    currentPage = 0;
-                }
-                mPager.setCurrentItem(currentPage++, true);
-            }
-        };
-        Timer swipeTimer = new Timer();
-        swipeTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(Update);
-            }
-        }, 3000, 3000);
-
-        // Pager listener over indicator
-        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageSelected(int position) {
-                currentPage = position;
-
-            }
-
-            @Override
-            public void onPageScrolled(int pos, float arg1, int arg2) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int pos) {
-
             }
         });
 
